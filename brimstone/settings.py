40dee3d64f78755f6bcf7dc898bdea2da05100cc
Thane Brimhall <thane.brimhall@gmail.com>
2016-03-29 22:13:05 -0600

    Added media and static directories

--- brimstone/settings.py
+++ brimstone/settings.py
@@ -128,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'brimstone', 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'brimstone', 'media')

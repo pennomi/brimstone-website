b8dd8eb276d0189e60a7b52481da1bf2bc23167f
Thane Brimhall <thane.brimhall@gmail.com>
2016-04-08 10:26:42 -0600

    Applied basic styling to comments

--- apps/cards/models.py
+++ apps/cards/models.py
@@ -157,11 +157,11 @@ class CardRevision(models.Model):
 class CardComment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments')
     card = models.ForeignKey(Card, related_name='comments')
-    text = models.CharField(max_length=1024)
+    text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('-created_at', )
+        ordering = ('created_at', )
 
     def __str__(self):
         return self.text

726a6417d723922f8eef5cefa9d246537fcc9257
Thane Brimhall <tbrimhall@izeni.com>
2016-04-12 13:41:33 -0600

    Begin refactoring jinja code to use filters and serializer data

--- apps/cards/renderer.py
+++ apps/cards/renderer.py
@@ -6,51 +6,55 @@ import os
 import tempfile
 
 from django.conf import settings
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 
 
-def _replace_markup(card, key):
+# Template Filters
+def jsonify(s):
+    return json.dumps(s)
+
+
+def markup(s):
     """Replaces [] with a background color tag and {} with fontawesome icons.
     """
     # [] Tags
-    card[key] = card[key].replace(
+    s = s.replace(
         '[', '<span font="DroidSans Bold 20" rise="1000" '
              'background="#212121" foreground="#FFFFFF"'
              'gravity="south"> '
     ).replace(']', ' </span>')
 
     # {} Tags
-    card[key] = card[key].replace(
+    s = s.replace(
         '{', '<span font="FontAwesome Normal">').replace('}', '</span>')
+    return s
 
 
+# Driver
 def generate_image(revision):
-    # Convert the revision into something we care about using
-    card = dict(
+    # Use the serializer to get a JSON representation of the card
+    from apps.cards.serializers import CardRevisionSerializer
+    s = CardRevisionSerializer(revision)
+    card = s.data.copy()
+
+    # Overwrite values strategically
+    # TODO: These should be template filters
+    card.update(
         id=str(revision.id).rjust(3, "0"),
         background=revision.type.background.image.path,
         image=revision.art.image.path if revision.art else "",
-        artist="{}" + revision.art.artist.name if revision.art else "",
-        stats=revision.stats,
-        title=revision.name,
         subtitle=revision.type.name,
         description=revision.description.replace('\n', '\\n'),
-        deck="{}",
-        table=json.dumps(revision.table),
-        table_y=revision.table_y,
     )
 
-    # Parse Markup
-    _replace_markup(card, 'description')
-    # _replace_markup(card, 'table')
-    _replace_markup(card, 'artist')
-    _replace_markup(card, 'deck')
+    # Set up Jinja
 
     # Load the template
-    template_file = os.path.join(
-        settings.BASE_DIR, 'apps', 'cards', 'templates', 'Portrait.tml')
-    with open(template_file, 'r') as infile:
-        template = Template(infile.read())
+    env = Environment(loader=FileSystemLoader(
+        os.path.join(settings.BASE_DIR, 'apps', 'cards', 'templates')))
+    env.filters['jsonify'] = jsonify
+    env.filters['markup'] = markup
+    template = env.get_template('Portrait.tml')
 
     # Render the image using squib
     # TODO: Can we do this without subprocess?

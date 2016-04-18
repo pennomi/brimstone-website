d0a4c6d012b732b47c0296baa4f6bbd313ca1faf
Thane Brimhall <thane.brimhall@gmail.com>
2016-04-18 10:51:30 -0600

    Progress on correctly binding the table editor

--- frontend/app/cards/card-table-field.component.ts
+++ frontend/app/cards/card-table-field.component.ts
@@ -3,34 +3,53 @@ import {Component, Input, Output, EventEmitter} from 'angular2/core';
 
 // TODO: This fails spectacularly if the input is not an array.
 @Component({
-    selector: 'card-table-field',
-    templateUrl: 'app/cards/card-table-field.component.html'
+  selector: 'card-table-field',
+  templateUrl: 'app/cards/card-table-field.component.html'
 })
 export class CardTableFieldComponent {
-    @Input() table: any;
-    @Output() tableChanged: EventEmitter<any> = new EventEmitter();
+  @Input() table: any;
+  @Output() tableChanged: EventEmitter<any> = new EventEmitter();
 
-    tableCopy: string[][] = [];
+  getTableWidthValues() {
+    return Array(this.table.length).fill(0).map((x,i)=>i);
+  }
 
-    ngOnInit() {
-        if (!this.table) {
-            this.table = [];
-        }
-        this.tableCopy = JSON.parse(JSON.stringify(this.table));
+  getTableHeightValues() {
+    let row = this.table[0]
+    return Array(row.length).fill(0).map((x,i)=>i);
+  }
+
+  addRow() {
+    var width = 1;
+    if (this.table[0]) {
+      width = Math.max(this.table[0].length, 1);
+    } else {
+      this.table = [];
+      this.tableChanged.emit(this.table);
+    }
+    this.table.push(Array(width).fill("TODO"));
+    this.tableChanged.emit(this.table);
+  }
+
+  addColumn() {
+    if (this.table.length == 0) {
+      this.addRow();
+      return;
     }
 
-    valueChanged(r, c, value) {
-        this.table[r][c] = value;
-        this.tableChanged.emit(this.table);
+    for (let row of this.table) {
+      row.push("TODO");
     }
+    this.tableChanged.emit(this.table);
+  }
 
-    addRow() {
-        var width = 1;
-        if (this.table[0]) {
-            width = Math.min(this.table[0].length, 1);
-        }
-        this.table.push(Array(width).fill(""));
-        this.tableCopy.push(Array(width).fill(""));
-        this.tableChanged.emit(this.table);
+  removeRow(i) {
+    this.table.splice(i, 1);
+  }
+
+  removeColumn(j) {
+    for (let row of this.table) {
+      row.splice(j, 1);
     }
+  }
 }
